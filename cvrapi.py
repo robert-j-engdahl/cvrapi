118acbb12b30c33408d30ef0f2e4345dbc84424a
Robert Jørgensgaard Engdahl <robert.j.engdahl@gmail.com>
2014-10-16 23:27:12 +0200

    python example for how to use cvrapi.dk

---
+++ cvrapi.py
@@ -0,0 +1,17 @@
+import urllib2
+import json
+import contextlib
+
+
+def cvrapi(cvr, country='dk'):
+    request = urllib2.Request(
+        url='http://cvrapi.dk/api?search=%d&country=%s' % (cvr, country),
+        headers={
+            'User-Agent': 'Robert Engdahl - test cvrapi via Python/Django.'})
+    with contextlib.closing(urllib2.urlopen(request)) as response:
+        return json.loads(response.read())
+
+
+# test cvrapi
+print(cvrapi(10150817))
+print(cvrapi(988615625, country='no'))
